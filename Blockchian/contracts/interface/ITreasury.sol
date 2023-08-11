a5e35ebb2ed741162c91a6e647563d95439c570f
4Kirubha <jaimelannister4221@gmail.com>
2023-08-11 17:55:11 +0530

    removed borrowerIndex in return

--- Blockchian/contracts/interface/ITreasury.sol
+++ Blockchian/contracts/interface/ITreasury.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.18;
 
 interface ITreasury{
-        function deposit(address user,uint64 _ethPrice,uint64 _depositTime) external payable returns(uint64,bool);
+        function deposit(address user,uint64 _ethPrice,uint64 _depositTime) external payable returns(bool);
         //function getUserAccountData (address user) external view returns();
 
         event Deposit(address indexed user,uint256 amount);
