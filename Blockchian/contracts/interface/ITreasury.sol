3d94835cfdd91db66de8fd818403e30f6b71b76f
4Kirubha <jaimelannister4221@gmail.com>
2023-09-01 17:32:37 +0530

    added interfaces

--- Blockchian/contracts/interface/ITreasury.sol
+++ Blockchian/contracts/interface/ITreasury.sol
@@ -3,15 +3,18 @@
 pragma solidity 0.8.18;
 
 interface ITreasury{
-        function deposit(address user,uint128 _ethPrice,uint64 _depositTime) external payable returns(bool);
+        function deposit(address user,uint128 _ethPrice,uint64 _depositTime) external payable returns(bool,uint64);
+        function withdraw(address toAddress,uint256 _amount) external;
         function depositToAave() external;
         function withdrawFromAave(uint64 index,uint256 amount) external;
         function depositToCompound() external;
         function withdrawFromCompound(uint64 index) external;
         function getBalanceInTreasury() external view returns(uint256);
-        function noOfBorrowers() external returns(uint128);
 
-        //function getUserAccountData (address user) external view returns();
+        function noOfBorrowers() external returns(uint128);
+        function totalVolumeOfBorrowersAmountinWei() external returns(uint256);
+        function totalVolumeOfBorrowersAmountinUSD() external returns(uint256);
+        function totalInterest() external returns(uint256);
 
         event Deposit(address indexed user,uint256 amount);
         event Withdraw(address indexed user,uint256 amount);
