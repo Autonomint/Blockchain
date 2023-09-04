3f9a56e72b642dbc3747a88c536e993cfc8e6176
4Kirubha <jaimelannister4221@gmail.com>
2023-09-04 15:21:02 +0530

    modified mint

--- Blockchian/contracts/Token/Protocol_Token.sol
+++ Blockchian/contracts/Token/Protocol_Token.sol
@@ -19,8 +19,9 @@ contract ProtocolToken is ERC20, ERC20Burnable, Pausable, Ownable {
         _unpause();
     }
 
-    function mint(address to, uint256 amount) public {
+    function mint(address to, uint256 amount) public returns(bool){
         _mint(to, amount);
+        return true;
     }
 
     function _beforeTokenTransfer(address from, address to, uint256 amount)
--- Blockchian/contracts/interface/IProtocolToken.sol
+++ Blockchian/contracts/interface/IProtocolToken.sol
@@ -3,7 +3,7 @@ pragma solidity 0.8.18;
 
 interface IProtocolToken {
     function balanceOf(address account) external view returns (uint256);
-    function mint(address to, uint256 amount) external;
+    function mint(address to, uint256 amount) external returns(bool);
     function burnFrom(address account, uint256 amount) external;
     function transferFrom(address from, address to, uint256 amount) external returns (bool);
     function transfer(address to,uint256 value) external returns(bool);
