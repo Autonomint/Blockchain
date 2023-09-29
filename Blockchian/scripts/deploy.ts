f06a3f87b5dfb81d397f5782ba04f112e144f742
4Kirubha <jaimelannister4221@gmail.com>
2023-09-29 11:50:24 +0530

    altered deploy script

--- Blockchian/scripts/deploy.ts
+++ Blockchian/scripts/deploy.ts
@@ -14,53 +14,58 @@ async function main() {
   await deployedTrinityStablecoin.deployed();
   console.log("TRINITY ADDRESS",deployedTrinityStablecoin.address);
 
-  await hre.run("verify:verify", {
-    address: deployedTrinityStablecoin.address,
-  });
-
   const ProtocolToken = await ethers.getContractFactory("ProtocolToken");
   const deployedProtocolToken = await ProtocolToken.deploy();
   await deployedProtocolToken.deployed();
   console.log("DIRAC ADDRESS",deployedProtocolToken.address);
-
-  await hre.run("verify:verify", {
-    address: deployedProtocolToken.address,
-  });
 
   const CDS = await ethers.getContractFactory("CDSTest");
   const deployedCDS = await CDS.deploy(deployedTrinityStablecoin.address,priceFeedAddress);
   await deployedCDS.deployed();
   console.log("CDS ADDRESS",deployedCDS.address);
 
-  await hre.run("verify:verify", {
-    address: deployedCDS.address,
-    constructorArguments: [deployedTrinityStablecoin.address,priceFeedAddress],
-  });
-
-
   const Borrowing = await ethers.getContractFactory("BorrowingTest");
   const deployedBorrowing = await Borrowing.deploy(deployedTrinityStablecoin.address,deployedCDS.address,deployedProtocolToken.address,priceFeedAddress);
   await deployedBorrowing.deployed();
   console.log("BORROWING ADDRESS",deployedBorrowing.address);
-
-  await hre.run("verify:verify", {
-    address: deployedBorrowing.address,
-    constructorArguments: [deployedTrinityStablecoin.address,deployedCDS.address,deployedProtocolToken.address,priceFeedAddress],
-  });
 
   const Treasury = await ethers.getContractFactory("Treasury");
   const deployedTreasury = await Treasury.deploy(deployedBorrowing.address,deployedTrinityStablecoin.address,deployedCDS.address,wethGateway,cEther,aavePoolAddress,aTokenAddress);
   await deployedTreasury.deployed();
   console.log("TREASURY ADDRESS",deployedTreasury.address);
 
+  async function sleep(ms:number) {
+    return new Promise((resolve) => setTimeout(resolve, ms));
+  }
+
+  await sleep(80 * 1000);
+
+  await hre.run("verify:verify", {
+    address: deployedTrinityStablecoin.address,
+    contract: "contracts/Token/Trinity_ERC20.sol:TrinityStablecoin"
+  });
+
+  await hre.run("verify:verify", {
+    address: deployedProtocolToken.address,
+    contract: "contracts/Token/Protocol_Token.sol:ProtocolToken"
+  });
+
+  await hre.run("verify:verify", {
+    address: deployedCDS.address,
+    contract: "contracts/TestContracts/CopyCDS.sol:CDSTest",
+    constructorArguments: [deployedTrinityStablecoin.address,priceFeedAddress],
+  });
+
+  await hre.run("verify:verify", {
+    address: deployedBorrowing.address,
+    contract: "contracts/TestContracts/CopyBorrowing.sol:BorrowingTest",
+    constructorArguments: [deployedTrinityStablecoin.address,deployedCDS.address,deployedProtocolToken.address,priceFeedAddress],
+  });
+
   await hre.run("verify:verify", {
     address: deployedTreasury.address,
     constructorArguments: [deployedBorrowing.address,deployedTrinityStablecoin.address,deployedCDS.address,wethGateway,cEther,aavePoolAddress,aTokenAddress],
   });
-
-  await deployedBorrowing.initializeTreasury(deployedTreasury.address);
-  await deployedBorrowing.setLTV(80);
-  await deployedCDS.setTreasury(deployedTreasury.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
@@ -71,11 +76,3 @@ main()
     console.error(error);
     process.exit(1);
   });
-
-
-
-// TRINITY   0x6F0a603bB5059DE0A1B7e5DA607C4dEe1C5EF73a
-// DIRAC     0x3161fa24895Da793f797c0B405B7D258DA0F4f88
-// CDS       0x96F44146714D6539FF64A31550d8f36aa94DA72b
-// BORROWING 0x79448092a379c1187945c8836Cb3EA2E2F6EEd08
-// TREASURY  0x101A6D6A73Bf1BE65371E90e8ce0bF467Dfff40A
